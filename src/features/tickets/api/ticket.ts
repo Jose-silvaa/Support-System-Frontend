37e318f19592344e9161e1aeebc64fb81e833ee4
Jose-silvaa <jvitorfacanha@gmail.com>
2026-08-12 19:15:22 -0300

    feat: usuário e tempo do histórico do ticket

--- src/features/tickets/api/ticket.ts
+++ src/features/tickets/api/ticket.ts
@@ -2,8 +2,6 @@ import type {TicketHistory} from "@/features/tickets/types/ticket.types.ts";
 import {get} from "@/lib/api.ts";
 
 export async function getHistoryTicket(ticketId: string): Promise<TicketHistory[]> {
-    const data = await get<TicketHistory[]>(`TicketHistory/${ticketId}`);
+    return await get<TicketHistory[]>(`TicketHistory/${ticketId}`);
 
-    console.log("ticket history data:", data);
-    return data;
 }
